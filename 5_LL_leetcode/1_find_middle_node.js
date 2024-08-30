85c4884afabea6908632d8e7e1f63e457f47bdca
Jonnaa <jonnaa96@yahoo.com>
2024-08-29 21:42:44 -0600

    Leetcode problem 1 complete

--- 5_LL_leetcode/1_find_middle_node.js
+++ 5_LL_leetcode/1_find_middle_node.js
@@ -71,12 +71,70 @@ class LinkedList {
     }
   }
 
-  // WRITE THE FINDMIDDLENODE METHOD HERE //
-  //                                      //
-  //                                      //
-  //                                      //
-  //                                      //
-  //////////////////////////////////////////
+  // My thinking
+  // Use a var that starts at 0
+  // That var =0 means its at head
+  // I will increment the var by 1 which will increase its distance from head
+  // I will then check if the distance from var to tail is the same
+  // Worked just like the actual solution, but solution is much nicer to look at and more efficient
+  //
+  findMiddleNode() {
+    if (!this.head) return null;
+    if (this.head === this.tail) return this.head;
+    const checkTail = (a, b) => {
+      let cc = 0;
+      while (a.next && cc <= b) {
+        cc++;
+        a = a.next;
+      }
+      return cc - b;
+    };
+    let count = 0;
+    let temp = this.head;
+    let result = 1;
+    let at = null;
+    let after;
+    while (temp !== this.tail && result != -1) {
+      result = checkTail(temp, count);
+      if (result === -1) after = temp;
+      if (result === 0) at = temp;
+      temp = temp.next;
+      count++;
+    }
+    if (at) return at;
+    else return after;
+  }
+
+  // --------------------
+  // ---- EXPLANATION ---
+  // --------------------
+  //   The findMiddleNode() function uses the "tortoise and hare" algorithm to find the middle node of a linked list.
+
+  // Here's a step-by-step explanation of the logic:
+
+  // Initialize two pointers, slow and fast, both pointing to the head of the linked list.
+
+  // Traverse the linked list using a while loop. The loop continues as long as fast is not null (i.e., it has not reached the end of the list), and fast.next is also not null (i.e., there is at least one more node after the current fast node).
+
+  // Inside the loop, move the slow pointer one step forward (i.e., slow = slow.next) and the fast pointer two steps forward (i.e., fast = fast.next.next).
+
+  // Since the fast pointer moves twice as fast as the slow pointer, by the time the fast pointer reaches the end of the list or goes beyond it, the slow
+  // findMiddleNode() {
+  //   if (!this.head) return null;
+  //   if (this.head === this.tail) return this.head;
+  //   // Initialize slow and fast pointers at head
+  //   let slow = this.head;
+  //   let fast = this.head;
+  //   // Iterate through the list
+  //   while (fast !== null && fast.next !== null) {
+  //     // Move slow pointer one step
+  //     slow = slow.next;
+  //     // Move fast pointer two steps
+  //     fast = fast.next.next;
+  //   }
+  //   // Return middle node when fast reaches end
+  //   return slow;
+  // }
 }
 
 let myLinkedList = new LinkedList(1);
